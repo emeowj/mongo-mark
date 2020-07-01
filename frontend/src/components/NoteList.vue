6298a0c6673f11b46253cc7a1e033d7f21ae7cdd
emeowj <git@meow.dev>
2020-06-30 17:21:27 -0700

    Add note list.

---
+++ frontend/src/components/NoteList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="note in notes" :key="note.id">
+        <note-list-item :note="note" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import NoteListItem from './NoteListItem.vue';
+
+export default {
+  components: {
+    NoteListItem,
+  },
+  data: function() {
+    return {
+      notes: [],
+    };
+  },
+  created: function() {
+    fetch('http://localhost:3000/notes')
+      .then((res) => res.json())
+      .then((data) => {
+        this.notes = data;
+      });
+  },
+};
+</script>
+
+<style></style>
---
+++ frontend/src/components/NoteListItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3>{{ note.title }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    note: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
