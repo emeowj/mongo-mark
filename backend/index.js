c9f5cbf70f3d110a119a3dcae572f58af7b94d23
emeowj <git@meow.dev>
2020-07-01 20:15:56 -0700

    Implement CRUD operation on note.

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,22 @@
 const Koa = require('koa');
 const Router = require('@koa/router');
 const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
+const { Note } = require('./model.js');
 
 const app = new Koa();
 app.use(bodyParser());
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('connected to: ' + db.name);
+});
 
 if (process.env.NODE_ENV === 'development') {
   app.use(async (context, next) => {
@@ -16,93 +29,75 @@ if (process.env.NODE_ENV === 'development') {
 
 const router = new Router();
 
-// Used to store all notes in meomry
-const notes = new Map();
-
-// Add some test notes so we don't get an empty list when nodemon restarts
-// the application.
-if (process.env.NODE_ENV === 'development') {
-  const now = new Date().getTime();
-  for (i in [1, 2, 3]) {
-    notes.set(`test-note-${i}`, {
-      title: 'Test note',
-      content: 'hello',
-      created: now,
-      updated: now,
+router.get('/notes', async (ctx) => {
+  await Note.find({}, 'title content created updated')
+    .exec()
+    .then((notes) => {
+      ctx.status = 200;
+      ctx.body = notes;
     });
-  }
-}
-
-router.get('/notes', (ctx, next) => {
-  ctx.body = Array.from(notes.values());
 });
 
 const noteRouter = new Router({
   prefix: '/note',
 });
 noteRouter
-  .get('/:note_id', (ctx, next) => {
+  .get('/:note_id', async (ctx) => {
     const id = ctx.params.note_id;
-    if (notes.has(id)) {
-      ctx.body = notes.get(id);
-    } else {
-      ctx.status = 404;
-      ctx.body = {
-        error: `Note with id ${id} does not exits`,
-      };
-    }
+    await Note.findById(id)
+      .exec()
+      .then((note) => {
+        if (!note) ctx.throw(404, `no note with id ${id}`);
+        ctx.status = 200;
+        ctx.body = note;
+      });
   })
-  .put('/', (ctx, next) => {
+  .put('/', async (ctx) => {
     const request = ctx.request.body;
-    const now = new Date().getTime();
-    const note = {
-      id: `${notes.size}`,
+    const note = new Note({
       title: request.title || 'No title',
       content: request.content || '',
-      created: now,
+    });
+    await note
+      .save()
+      .exec()
+      .then((note) => {
+        if (!note) ctx.throw(404, `no note with id ${id}`);
+        console.log(`Created note ${note._id}`);
+        ctx.status = 201;
+        ctx.body = note;
+      });
+  })
+  .delete('/:note_id', async (ctx) => {
+    const id = ctx.params.note_id;
+    await Note.findByIdAndDelete(id)
+      .exec()
+      .then((note) => {
+        if (!note) ctx.throw(404, `no note with id ${id}`);
+        ctx.status = 204;
+        ctx.body = {};
+      });
+  })
+  .patch('/:note_id', async (ctx) => {
+    const id = ctx.params.note_id;
+    const now = new Date().getTime();
+    const request = await ctx.request.body;
+    const updates = {
       updated: now,
     };
-    notes.set(note.id, note);
-    console.log(`Created note ${note.id}`);
-    ctx.status = 201;
-    ctx.body = note;
-  })
-  .delete('/:note_id', (ctx, next) => {
-    const id = ctx.params.note_id;
-    if (notes.has(id)) {
-      notes.delete(id);
-      console.log(`Deleted note ${id}`);
-      ctx.status = 204;
-      ctx.body = {};
-    } else {
-      ctx.status = 404;
-      ctx.body = {
-        error: `Note with id ${id} does not exits`,
-      };
+    if (request.title) {
+      updates.title = request.title;
     }
-  })
-  .patch('/:note_id', (ctx, next) => {
-    const id = ctx.params.note_id;
-    if (notes.has(id)) {
-      const now = new Date().getTime();
-      const request = ctx.request.body;
-      const note = notes.get(id);
-      const updates = {
-        updated: now,
-        title: request.title || note.title,
-        content: request.content || note.content,
-      };
-      const updated = Object.assign(note, updates);
-      notes.set(id, updated);
-      console.log(`Updated note ${id}`);
-      ctx.status = 202;
-      ctx.body = updated;
-    } else {
-      ctx.status = 404;
-      ctx.body = {
-        error: `Note with id ${id} does not exits`,
-      };
+    if (request.content) {
+      updates.content = request.content;
     }
+    await Note.findByIdAndUpdate(id, updates)
+      .exec()
+      .then((note) => {
+        if (!note) ctx.throw(404, `no note with id ${id}`);
+        ctx.status = 202;
+        ctx.body = note;
+      });
   });
 
 router.use(noteRouter.routes());
