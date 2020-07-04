30b3d6961ad7e1eb519357536c2c1bc094fdef53
emeowj <git@meow.dev>
2020-07-04 15:58:27 -0700

    handle click and esc key event in NoteListItem.

--- frontend/src/components/NoteListItem.vue
+++ frontend/src/components/NoteListItem.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="relative m-w-32" :class="{ selected: selected }">
     <div
-      v-if="confirmDelete"
+      v-if="state === State.DELETING"
       class="delete-buttons absolute w-full h-full flex justify-around items-center"
     >
-      <button class="btn text-white" @click.stop="confirmDelete = false">Cancel</button>
-      <button class="btn text-orange-500" @click.stop="deleteNote">Delete</button>
+      <button class="btn text-white" @click.stop="state = State.DELETING">
+        Cancel
+      </button>
+      <button class="btn text-orange-500" @click.stop="deleteNote">
+        Delete
+      </button>
     </div>
     <div class="item flex justify-between p-2 border" @dblclick="editNote">
       <div class="flex flex-col items-start justify-between">
         <input
-          v-if="editing"
+          v-if="state === State.EDITING"
           class="title-input"
           type="text"
           v-model="newTitle"
           @blur="updateNote"
           @keyup.enter="updateNote"
+          @click.stop
         />
         <div v-else class="text-md font-bold">{{ note.title }}</div>
         <p class="text-gray-700 text-sm">
@@ -24,10 +29,10 @@
         </p>
       </div>
       <div
-        v-if="!editing"
+        v-if="state !== State.EDITING"
         class="menu invisible hover:visible flex items-center text-gray-400 text-sm"
       >
-        <button @click.stop="confirmDelete = true">
+        <button @click.stop="state = State.DELETING">
           <i class="fas fa-trash"></i>
         </button>
       </div>
@@ -42,43 +47,73 @@ export default {
   props: {
     note: {
       type: Object,
-      required: true
+      required: true,
     },
     selected: {
       type: Boolean,
-      default: false
-    }
+      default: false,
+    },
+  },
+  watch: {
+    state: function(val) {
+      switch (val) {
+        case this.State.EDITING:
+        case this.State.DELETING:
+          window.addEventListener('click', this.clickEventHandler);
+          window.addEventListener('keydown', this.escEventHandler);
+          break;
+        case this.State.DEFAULT:
+          window.removeEventListener('click', this.clickEventHandler);
+          window.removeEventListener('keydown', this.escEventHandler);
+      }
+    },
   },
   data: function() {
     return {
-      confirmDelete: false,
-      editing: false,
-      newTitle: this.note.title
+      State: Object.freeze({
+        DEFAULT: 'default',
+        DELETING: 'deleting',
+        EDITING: 'editing',
+      }),
+      state: 'default',
+      newTitle: this.note.title,
+      clickEventHandler: (ev) => {
+        if (ev.target != this.$el.querySelector('.delete-buttons')) {
+          this.state = this.State.DEFAULT;
+        }
+      },
+      escEventHandler: (ev) => {
+        if (ev.keyCode === 27) {
+          this.state = this.State.DEFAULT;
+          this.newTitle = this.note.title;
+        }
+      },
     };
   },
   computed: {
     updated() {
       return moment(this.note.updated).fromNow();
-    }
+    },
   },
   methods: {
     deleteNote() {
       this.$emit('deleteNote', this.note._id);
-      this.confirmDelete = false;
+      this.state = this.State.DELETING;
     },
     editNote() {
-      this.editing = true;
+      this.state = this.State.EDITING;
       this.$nextTick(() => {
         this.$el.querySelector('.title-input').focus();
       });
     },
     updateNote() {
-      this.editing = false;
+      this.state = this.State.DEFAULT;
       if (this.newTitle != this.note.title) {
+        this.note.title = this.newTitle;
         this.$emit('updateNote', { id: this.note._id, title: this.newTitle });
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
